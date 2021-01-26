43fde54106b3e7a00f28e6ee3ebb1441031bf21d
Jeremiah Ang <jeremiahang94@gmail.com>
2021-01-26 08:10:25 +0800

    Initial

---
+++ Assets/DetachFromParent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetachFromParent : MonoBehaviour
+{
+
+    [SerializeField] GameObject referenceGameObject;
+    private bool isAttached = true;
+    public void Detach()
+    {
+        Debug.Log("Snapppp");
+        if (!isAttached) return;
+        isAttached = false;
+        referenceGameObject.transform.parent = referenceGameObject.transform.parent.transform.parent;
+    }
+}
---
+++ Assets/OnCollisionWith.cs
@@ -0,0 +1,25 @@
+﻿namespace Survival
+{
+    using UnityEngine.Events;
+    using UnityEngine;
+    using System;
+
+    public class OnCollisionWith : MonoBehaviour
+    {
+        [SerializeField] GameObject collideWith;
+
+        [Serializable]
+        public class OnCollision : UnityEvent<bool>
+        {
+        }
+        [SerializeField] OnCollision onCollision = new OnCollision();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject == collideWith)
+            {
+                onCollision.Invoke(true);
+            }
+        }
+    }
+}
